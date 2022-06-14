2ef2bbbcf6b39d5e8c8afefaface995590288ab0
Adel SATTARI <adel.sattari@gmail.com>
2022-06-14 20:10:42 +0200

    Searching for a movie

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -22,7 +22,22 @@ namespace eTickets.Controllers
         public  async Task<IActionResult> Index()
         {
             var allMovies =  await _service.GetAllAsync(n =>n.Cinema);
+
+
             return View(allMovies);
+        }
+
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allMovies = await _service.GetAllAsync(n => n.Cinema);
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResult = allMovies.Where(n => n.Name.Contains(searchString) || n.Description.Contains
+                (searchString)).ToList();
+                return View("Index", filteredResult);
+            }
+            return View("Index", allMovies);
         }
 
         //GET : Movies/Details/1
