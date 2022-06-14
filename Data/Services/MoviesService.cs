85b2d5f9a9bb05924633320cc4c687c78950058d
Adel SATTARI <adel.sattari@gmail.com>
2022-06-14 07:43:24 +0200

    Adding Dropdown lists from database

--- Data/Services/MoviesService.cs
+++ Data/Services/MoviesService.cs
@@ -1,4 +1,5 @@
 ﻿using eTickets.Data.Base;
+using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -8,16 +9,16 @@ using System.Threading.Tasks;
 
 namespace eTickets.Data.Services
 {
-    public class MoviesService:EntityBaseRepository<Movie>, IMoviesService
+    public class MoviesService : EntityBaseRepository<Movie>, IMoviesService
     {
         private readonly AppDbContext _context;
-        public MoviesService(AppDbContext context):base(context)
+        public MoviesService(AppDbContext context) : base(context)
         {
             _context = context;
 
         }
 
-        public async  Task<Movie> GetMovieByIdAsync(int id)
+        public async Task<Movie> GetMovieByIdAsync(int id)
         {
             var movieDetails = await _context.Movies
             .Include(c => c.Cinema)
@@ -25,8 +26,20 @@ namespace eTickets.Data.Services
             .Include(am => am.Actor_Movies).ThenInclude(a => a.Actor)
             .FirstOrDefaultAsync(n => n.Id == id);
 
-            return  movieDetails;
+            return movieDetails;
 
+        }
+
+        public async Task<NewMovieDropdownsVM> GetNewMovieDropDownsValues()
+        {
+            var response = new NewMovieDropdownsVM()
+            {
+                Actors = await _context.Actors.OrderBy(n => n.FullName).ToListAsync(),
+                Cinemas = await _context.Cinemas.OrderBy(n => n.Name).ToListAsync(),
+                Producers = await _context.Producers.OrderBy(n => n.FullName).ToListAsync()
+            };
+
+            return response;
         }
     }
 }
