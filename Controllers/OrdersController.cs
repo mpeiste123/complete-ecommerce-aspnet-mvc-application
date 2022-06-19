f7bb6b657dfc822fa380e41af20e440abbe41cd2
Adel SATTARI <adel.sattari@gmail.com>
2022-06-19 17:46:39 +0200

    Role based Orders View

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace eTickets.Controllers
@@ -23,8 +24,11 @@ namespace eTickets.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            string userId = "";
-            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+            //Claim types 
+            //The NameIdentifier is used for the userId
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userRole = User.FindFirstValue(ClaimTypes.Role);
+            var orders = await _ordersService.GetOrdersByUserIdAndRoleAsync(userId, userRole);
             return View(orders);
 
         }
@@ -61,8 +65,8 @@ namespace eTickets.Controllers
         public async  Task<IActionResult> CompleteOrder()
         {
             var items = _shoppingCart.GetShoppingCartItems();
-            string userId = "";
-            string userEmailAddress = "";
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userEmailAddress =User.FindFirstValue(ClaimTypes.Email);
            await  _ordersService.StoreOrderAsync(items, userId, userEmailAddress);
 
             //Clean up the shopping cart
